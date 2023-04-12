5cfcb755a2462c4a0c34c054c9282f931d636d34
PFK0815 <130490271+PFK0815@users.noreply.github.com>
2023-04-12 19:09:41 +0200

    Schneller WebSite Ersteller

---
+++ CreateWebSite.sh
@@ -0,0 +1,43 @@
+# Autor: Programmieren für Kinder
+
+echo "Schneller WebSite Ersteller"
+echo "Autor: Programmieren für Kinder"
+echo ""
+echo "Der Oberordner des Projekts ist in dieser Datei änderbar."
+
+read -p "Wie soll die Website heißen(Dies ist der Titel und der Speicherort im Oberverzeichnis): " name
+
+# Hier ist das Oberverzeichnis des Projekts
+cd ~/Dokumente/PFK/
+
+if [ -d "$name" ];then
+    # Projekt existiert schon im Zielort
+    echo "Projekt existiert schon im Zielordner!"
+    echo "Erstellung wurde abgebrochen"
+    exit 1
+fi
+
+# Ab hier wird das Projekt mit den wichtigsten Dateien einer WebSite geschrieben.
+mkdir $name
+cd $name
+echo "<!-- HTML Document -->
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>$name</title>
+		<link rel=\"stylesheet\" href=\"style.css\">
+		<script src=\"script.js\"></script>
+	</head>
+	<body>
+		
+	</body>
+</html>" > index.html
+echo "/* Javascript Document */
+document.addEventListener(\"DOMContentLoaded\",function(e){
+	
+});" > script.js
+echo "/* Style Document */
+" > style.css
+
+#Wenn du Visual Studio Code hast, dann aktiviere diesen Befehl welcher die App dann gleich im Projekt öffnet:
+#code ./
